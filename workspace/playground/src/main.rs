1a31919959fef3e85563ca45f953162a3c9ba90a
Jaeseo Lee <85179805+Tosaaa@users.noreply.github.com>
2023-12-07 14:51:15 +0000

    cleaned playground

--- workspace/playground/src/main.rs
+++ workspace/playground/src/main.rs
@@ -1,27 +1,3 @@
-use std::cell::Cell;
-
-pub struct SpiderRobot {
-    hardware_error_count: Cell<u32>
-}
-
-impl SpiderRobot {
-    pub fn new() -> SpiderRobot {
-        SpiderRobot { hardware_error_count: Cell::new(0) }
-    }
-
-    pub fn add_hardware_error(&self) {
-        let n = self.hardware_error_count.get();
-        self.hardware_error_count.set(n + 1);
-    }
-
-    pub fn has_hardware_errors(&self) -> bool {
-        self.hardware_error_count.get() > 0
-    }
-}
-
 fn main() {
-    let robot = SpiderRobot::new();
-    println!("{}", robot.has_hardware_errors());
-    robot.add_hardware_error();
-    println!("{}", robot.has_hardware_errors());
+    
 }
