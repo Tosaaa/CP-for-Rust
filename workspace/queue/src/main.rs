cdae08ea217eda22dff4f9716f56353ef7cfc503
Jaeseo Lee <85179805+Tosaaa@users.noreply.github.com>
2023-12-07 14:00:55 +0000

    generic queue implementation

--- workspace/queue/src/main.rs
+++ workspace/queue/src/main.rs
@@ -1,18 +1,18 @@
-pub struct Queue {
-    older: Vec<char>,
-    younger: Vec<char>
+pub struct Queue<T> {
+    older: Vec<T>,
+    younger: Vec<T>
 }
 
-impl Queue {
-    pub fn new() -> Queue { 
+impl<T> Queue<T> {
+    pub fn new() -> Self { 
        Queue{ older: Vec::new(), younger: Vec::new() }
     }
     
-    pub fn push(&mut self, c: char) {
-        self.younger.push(c);
+    pub fn push(&mut self, t: T) {
+        self.younger.push(t);
     }
 
-    pub fn pop(&mut self) -> Option<char> {
+    pub fn pop(&mut self) -> Option<T> {
         if self.older.is_empty() {
             if self.younger.is_empty() {
                 return None;
@@ -30,19 +30,18 @@ impl Queue {
         self.older.is_empty() && self.younger.is_empty()
     }
 
-    pub fn split(self) -> (Vec<char>, Vec<char>) {
+    pub fn split(self) -> (Vec<T>, Vec<T>) {
         (self.older, self.younger)
     }
 }
 
 fn main() {
     let mut q = Queue::new();
-    q.push('P');
-    q.push('D');
-    assert_eq!(q.pop(), Some('P'));
-    q.push('X');
+    let mut r = Queue::new();
 
-    let (older, younger) = q.split();
-    assert_eq!(older, vec!['D']);
-    assert_eq!(younger, vec!['X']);
+    q.push("CAD"); // Queue<&'static str>
+    r.push(0.74); // Queue<f64>
+
+    q.push("BTC");
+    r.push(13764.0);
 }
