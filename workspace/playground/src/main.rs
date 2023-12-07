c35ab0f572526bacd071fe784b5aa2b172b3be5e
Jaeseo Lee <85179805+Tosaaa@users.noreply.github.com>
2023-12-07 14:44:48 +0000

    cell implementation

--- workspace/playground/src/main.rs
+++ workspace/playground/src/main.rs
@@ -1,40 +1,27 @@
-struct Broom {
-    name: String,
-    height: u32,
-    health: u32,
-    position: (f32, f32, f32),
-    intent: BroomIntent
+use std::cell::Cell;
+
+pub struct SpiderRobot {
+    hardware_error_count: Cell<u32>
 }
 
-#[derive(Copy, Clone)]
-enum BroomIntent { FetchWater, DumpWater }
+impl SpiderRobot {
+    pub fn new() -> SpiderRobot {
+        SpiderRobot { hardware_error_count: Cell::new(0) }
+    }
 
-fn chop(b: Broom) -> (Broom, Broom) {
-    let mut broom1 = Broom { height: b.height / 2, .. b };
+    pub fn add_hardware_error(&self) {
+        let n = self.hardware_error_count.get();
+        self.hardware_error_count.set(n + 1);
+    }
 
-    let mut broom2 = Broom { name: broom1.name.clone(), .. broom1 };
-
-    broom1.name.push_str(" I");
-    broom2.name.push_str(" II");
-
-    (broom1, broom2)
+    pub fn has_hardware_errors(&self) -> bool {
+        self.hardware_error_count.get() > 0
+    }
 }
 
 fn main() {
-    let hokey = Broom {
-        name: "Hokey".to_string(),
-        height: 60,
-        health: 100,
-        position: (100.0, 200.0, 0.0),
-        intent: BroomIntent::FetchWater
-    };
-
-    let (hokey1, hokey2) = chop(hokey);
-    assert_eq!(hokey1.name, "Hokey I");
-    assert_eq!(hokey1.height, 30);
-    assert_eq!(hokey1.health, 100);
-
-    assert_eq!(hokey2.name, "Hokey II");
-    assert_eq!(hokey2.height, 30);
-    assert_eq!(hokey2.health, 100);
+    let robot = SpiderRobot::new();
+    println!("{}", robot.has_hardware_errors());
+    robot.add_hardware_error();
+    println!("{}", robot.has_hardware_errors());
 }
