98c8ba2c84cce8000beb1e83792e95a4ef104b69
Jaeseo Lee <85179805+Tosaaa@users.noreply.github.com>
2023-12-07 13:49:30 +0000

    added queue, playground

---
+++ workspace/queue/src/main.rs
@@ -0,0 +1,48 @@
+pub struct Queue {
+    older: Vec<char>,
+    younger: Vec<char>
+}
+
+impl Queue {
+    pub fn new() -> Queue { 
+       Queue{ older: Vec::new(), younger: Vec::new() }
+    }
+    
+    pub fn push(&mut self, c: char) {
+        self.younger.push(c);
+    }
+
+    pub fn pop(&mut self) -> Option<char> {
+        if self.older.is_empty() {
+            if self.younger.is_empty() {
+                return None;
+            }
+
+            use std::mem::swap;
+            swap(&mut self.older, &mut self.younger);
+            self.older.reverse();
+        }
+        
+        self.older.pop()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.older.is_empty() && self.younger.is_empty()
+    }
+
+    pub fn split(self) -> (Vec<char>, Vec<char>) {
+        (self.older, self.younger)
+    }
+}
+
+fn main() {
+    let mut q = Queue::new();
+    q.push('P');
+    q.push('D');
+    assert_eq!(q.pop(), Some('P'));
+    q.push('X');
+
+    let (older, younger) = q.split();
+    assert_eq!(older, vec!['D']);
+    assert_eq!(younger, vec!['X']);
+}
