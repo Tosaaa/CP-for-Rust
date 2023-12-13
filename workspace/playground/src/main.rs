93050929fcabc88340df0baafbab17338cba9494
Jaeseo Lee <85179805+Tosaaa@users.noreply.github.com>
2023-12-13 14:04:20 +0000

    Update playground

--- workspace/playground/src/main.rs
+++ workspace/playground/src/main.rs
@@ -1,60 +1,16 @@
-#[derive(Debug, Clone)]
-pub struct TimeError {
-    pub message: String,
-}
+pub struct Sink;
 
-use std::fmt;
-impl fmt::Display for TimeError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        write!(f, "{}", self.message)
+use std::io::{Write, Result};
+
+impl Write for Sink {
+    fn write(&mut self, buf: &[u8]) -> Result<usize> {
+        Ok(buf.len())
     }
-}
-
-impl std::error::Error for TimeError {}
-
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
-enum TimeUnit {
-    Seconds, Minutes, Hours, Days, Months, Years
-}
-
-impl TimeUnit {
-    fn new(unit: &str) -> Result<TimeUnit, TimeError> {
-        match unit {
-            "seconds" => Ok(TimeUnit::Seconds),
-            "minutes" => Ok(TimeUnit::Minutes),
-            "hours" => Ok(TimeUnit::Hours),
-            "days" => Ok(TimeUnit::Days),
-            "months" => Ok(TimeUnit::Months),
-            "years" => Ok(TimeUnit::Years),
-            _ => Err(TimeError { message: "unit not found.".to_string() })
-        }
-    }
-
-    fn plural(self) -> &'static str {
-        match self {
-            TimeUnit::Seconds => "seconds",
-            TimeUnit::Minutes => "minutes",
-            TimeUnit::Hours => "hours",
-            TimeUnit::Days => "days",
-            TimeUnit::Months => "months",
-            TimeUnit::Years => "years"
-        }
-    }
-
-    fn singular(self) -> &'static str {
-        self.plural().trim_end_matches("s")
+    fn flush(&mut self) -> Result<()> {
+        Ok(())
     }
 }
 
 fn main() {
-    
-}
 
-#[test]
-fn timeunit_test() -> Result<(), TimeError> {
-    let min = TimeUnit::new("minutes")?;
-    assert_eq!(min, TimeUnit::Minutes);
-    assert_eq!(min.plural(), "minutes");
-    assert_eq!(min.singular(), "minute");
-    Ok(())
 }
