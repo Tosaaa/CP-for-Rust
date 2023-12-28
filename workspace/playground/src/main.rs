0538fd5ecd496adc93849bb251ed2b217e4c6ed8
Jaeseo Lee <85179805+Tosaaa@users.noreply.github.com>
2023-12-28 13:59:47 +0000

    Change Playground (fizzbuzz)

--- workspace/playground/src/main.rs
+++ workspace/playground/src/main.rs
@@ -1,8 +1,18 @@
+use std::iter::{once, repeat};
+
 fn main() {
-    let text = "    ponies   \n   giraffes\niguanas    \nsquid".to_string();
-    let v: Vec<&str> = text.lines()
-        .map(str::trim)
-        .filter(|s| *s != "iguanas")
-        .collect();
-    assert_eq!(v, ["ponies", "giraffes", "squid"]);
+    let fizzes = repeat("").take(2).chain(once("fizz")).cycle();
+    let buzzes = repeat("").take(4).chain(once("buzz")).cycle();
+    let fizzes_buzzes = fizzes.zip(buzzes);
+
+    let fizz_buzz = (1..100).zip(fizzes_buzzes)
+        .map(|tuple| 
+            match tuple {
+                (i, ("", "")) => i.to_string(),
+                (_, (fizz, buzz)) => format!("{}{}", fizz, buzz)
+            });
+
+    for line in fizz_buzz {
+        println!("{}", line);
+    }
 }
