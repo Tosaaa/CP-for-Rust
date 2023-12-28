7a14c3055f0ec35c021e7ef5c994ed933c3c535d
Jaeseo Lee <85179805+Tosaaa@users.noreply.github.com>
2023-12-28 13:13:03 +0000

    Change playground (iterator)

--- workspace/playground/src/main.rs
+++ workspace/playground/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    
+    let text = "    ponies   \n   giraffes\niguanas    \nsquid".to_string();
+    let v: Vec<&str> = text.lines()
+        .map(str::trim)
+        .filter(|s| *s != "iguanas")
+        .collect();
+    assert_eq!(v, ["ponies", "giraffes", "squid"]);
 }
